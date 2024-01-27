dcec4da55ef9d10b939fc91ef00bc3b369770a96
Kiran Babu <kiranbabu3355@gmail.com>
2024-01-27 23:38:36 +0530

    Refactor nav-links.tsx

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -6,8 +6,9 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
+
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -39,7 +40,6 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-
           </Link>
 
         );
